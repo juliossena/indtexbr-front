abf5715a68492cb9d4dfa0657fcbb4b5214e0a96
juliossena <juliosouzasena@gmail.com>
2021-03-21 16:05:22 -0300

    feat: insert resposive in menu

--- src/shared/components/menu/Menu.tsx
+++ src/shared/components/menu/Menu.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import PeopleIcon from '@material-ui/icons/People';
 import HomeIcon from '@material-ui/icons/Home';
 import AllInboxIcon from '@material-ui/icons/AllInbox';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import { RootStateGlobal } from '../../../store/reducer';
 import { colors } from '../../functions/colors';
@@ -12,29 +13,38 @@ import ButtonMenu from '../buttons/buttonMenu/ButtonMenu';
 import {
     Container,
     Img,
+    ContainerIcon,
 } from './styles';
+import ButtonIcon from '../buttons/buttonIcon/ButtonIcon';
 
 const Menu = () => {
   const history = useHistory();
+  const [showMenu, setShowMenu] = useState(false);
   const { userData } = useSelector((state: RootStateGlobal) => state.userReducer);
 
   return (
-    <Container>
-      <Img src="/svg/logo.svg" alt="Logo" />
-      <ButtonMenu
-        onClick={() => history.push(MenuUrl.home)}
-        icon={(
-          <HomeIcon
-            htmlColor={window.location.pathname === MenuUrl.home
+    <>
+      <ContainerIcon>
+        <ButtonIcon onClick={() => setShowMenu(!showMenu)}>
+          <MenuIcon />
+        </ButtonIcon>
+      </ContainerIcon>
+      <Container showMenu={showMenu}>
+        <Img src="/svg/logo.svg" alt="Logo" />
+        <ButtonMenu
+          onClick={() => history.push(MenuUrl.home)}
+          icon={(
+            <HomeIcon
+              htmlColor={window.location.pathname === MenuUrl.home
             ? colors.yellowRegular
             : colors.white}
-          />
-)}
-        isSelected={window.location.pathname === MenuUrl.home}
-      >
-        HOME
-      </ButtonMenu>
-      { userData && userData.idTypeUser === 1 && (
+            />
+        )}
+          isSelected={window.location.pathname === MenuUrl.home}
+        >
+          HOME
+        </ButtonMenu>
+        { userData && userData.idTypeUser === 1 && (
         <ButtonMenu
           onClick={() => history.push(MenuUrl.user)}
           icon={(
@@ -50,20 +60,21 @@ const Menu = () => {
         </ButtonMenu>
       )}
 
-      <ButtonMenu
-        onClick={() => history.push(MenuUrl.product)}
-        icon={(
-          <AllInboxIcon
-            htmlColor={window.location.pathname === MenuUrl.product
+        <ButtonMenu
+          onClick={() => history.push(MenuUrl.product)}
+          icon={(
+            <AllInboxIcon
+              htmlColor={window.location.pathname === MenuUrl.product
               ? colors.yellowRegular
               : colors.white}
-          />
+            />
         )}
-        isSelected={window.location.pathname === MenuUrl.product}
-      >
-        PRODUTOS
-      </ButtonMenu>
-    </Container>
+          isSelected={window.location.pathname === MenuUrl.product}
+        >
+          PRODUTOS
+        </ButtonMenu>
+      </Container>
+    </>
   );
 };
 
--- src/shared/components/menu/styles.ts
+++ src/shared/components/menu/styles.ts
@@ -2,12 +2,32 @@ import styled from 'styled-components';
 
 import { colors } from '../../functions/colors';
 
-export const Container = styled.menu`
+interface PContainer {
+  showMenu: boolean;
+}
+
+export const Container = styled.menu<PContainer>`
     width: 192px;
     height: 100vh;
     background-color: ${colors.primary};
     display: flex;
     flex-direction: column;
+    z-index: 5;
+
+    @media (max-width: 600px) {
+      display: ${props => (props.showMenu ? 'flex' : 'none')};
+      position: absolute;
+    }
+`;
+
+export const ContainerIcon = styled.div`
+  display: none;
+  position: absolute;
+  padding: 8px;
+  z-index: 6;
+  @media (max-width: 600px) {
+    display: block;
+  }
 `;
 
 export const Img = styled.img`
